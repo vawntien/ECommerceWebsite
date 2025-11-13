5ea21f2a2c92dad350c7f0bfda99241dd3a76a32
vawntien <vawntien@gmail.com>
2025-11-13 16:17:39 +0700

    insert: NguoiBanController, LayoutNguoiBan

---
+++ ECommerceWebsiteMVC/Controllers/NguoiBanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceWebsiteMVC.Controllers
+{
+    public class NguoiBanController : Controller
+    {
+        // GET: NguoiBan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
