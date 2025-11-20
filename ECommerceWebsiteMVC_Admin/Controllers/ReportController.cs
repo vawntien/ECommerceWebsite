e61c9762e4f7aad310bf9a48ab30f04ceada79d6
DangNguyenHoai <dangnguyenhoai2n5@gmail.com>
2025-11-20 10:56:37 +0700

    Update layout admin

---
+++ ECommerceWebsiteMVC_Admin/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceWebsiteMVC_Admin.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
