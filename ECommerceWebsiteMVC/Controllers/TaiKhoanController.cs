a3f2b1521d7f2935026ed2c5a8627bace1ef9ae6
Nguyen Ngoc Thinh <thinhnguyenngoc0702@gmail.com>
2025-11-20 11:01:00 +0700

    Add view DangKy, DangNhap

---
+++ ECommerceWebsiteMVC/Controllers/TaiKhoanController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceWebsiteMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceWebsiteMVC.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        ECommerceWebsiteEntities eCommerceWebsiteEntities = new ECommerceWebsiteEntities();
+
+        // GET: TaiKhoan
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
