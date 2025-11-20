d8ffa45f3926557c6e38b810f56e87dd89829463
DangNguyenHoai <dangnguyenhoai2n5@gmail.com>
2025-11-21 00:50:48 +0700

    Add template

---
+++ ECommerceWebsiteMVC/Controllers/DanhGiaController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceWebsiteMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceWebsiteMVC.Controllers
+{
+    public class DanhGiaController : Controller
+    {
+        // GET: DanhGia
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult ThemDanhGia(DanhGiaSanPham model, HttpPostedFileBase HinhAnhFile)
+        {
+            // model.SoSao, model.TieuDe, model.NoiDung, model.MaSanPham... đã có
+            // HinhAnhFile: lưu file, lấy đường dẫn gán vào model.HinhAnh
+            // Lưu model vào DB...
+
+            // Sau đó redirect lại trang đơn mua
+            return RedirectToAction("DonMua");
+        }
+
+    }
+}
