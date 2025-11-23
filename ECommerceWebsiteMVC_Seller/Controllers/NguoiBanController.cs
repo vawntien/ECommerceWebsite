e89b99796ca97a7e156650fec1c8766ba16994c6
Tien Nguyen Van <nguyenvantien1122005@gmail.com>
2025-11-23 23:44:05 +0700

    insert: view DSSP for NGuoiBan

--- ECommerceWebsiteMVC_Seller/Controllers/NguoiBanController.cs
+++ ECommerceWebsiteMVC_Seller/Controllers/NguoiBanController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceWebsiteMVC_Seller.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace ECommerceWebsiteMVC.Controllers
     public class NguoiBanController : Controller
     {
         // GET: NguoiBan
+
+        ECommerceWebsiteEntities db = new ECommerceWebsiteEntities();
         public ActionResult Index()
         {
             return View();
@@ -17,5 +20,30 @@ namespace ECommerceWebsiteMVC.Controllers
         {
             return View();
         }
+
+        public ActionResult TatCaSanPham()
+        {
+            if (Session["MaNguoiDung"] == null)
+                return RedirectToAction("DangNhapNguoiBan", "TaiKhoan");
+
+            int maNguoiDung = (int)Session["MaNguoiDung"];
+
+            // 1. Lấy cửa hàng của người bán
+            var cuaHang = db.CuaHangs.SingleOrDefault(x => x.MaNguoiDung == maNguoiDung);
+
+            if (cuaHang == null)
+                return Content("Bạn chưa tạo cửa hàng.");
+
+            int maCuaHang = cuaHang.MaCuaHang;
+
+            // 2. Lấy sản phẩm thuộc cửa hàng đó
+            var dsSanPham = db.SanPhams
+                              .Where(sp => sp.MaCuaHang == maCuaHang)
+                              .ToList();
+
+            return View(dsSanPham);
+        }
+
+
     }
 }
