b028c801bbcd13b9ced9140022c8e5a0ab0b77fe
Tien Nguyen Van <nguyenvantien1122005@gmail.com>
2025-12-02 23:12:44 +0700

    update: fix TatCaSanPham and complete the funtions of CapNhatSanPham

--- ECommerceWebsiteMVC_Seller/Controllers/NguoiBanController.cs
+++ ECommerceWebsiteMVC_Seller/Controllers/NguoiBanController.cs
@@ -29,7 +29,7 @@ namespace ECommerceWebsiteMVC.Controllers
         public ActionResult TatCaSanPham()
         {
             //csdl
-
+            #region csdl
             //if (Session["MaNguoiBan"] == null)
             //    return RedirectToAction("DangNhapNguoiBan", "TaiKhoan");
 
@@ -40,49 +40,18 @@ namespace ECommerceWebsiteMVC.Controllers
             //).ToList();
 
             //return View(dsSanPham);
+            #endregion
 
-
-
-
-
-
-            //entity
             if (Session["MaNguoiBan"] == null)
                 return RedirectToAction("DangNhapNguoiBan", "TaiKhoan");
-
             int maNguoiBan = (int)Session["MaNguoiBan"];
-
-            // 1. Lấy cửa hàng của người bán
             var cuaHang = db.CuaHangs.SingleOrDefault(x => x.MaNguoiBan == maNguoiBan);
 
             if (cuaHang == null)
                 return Content("Bạn chưa tạo cửa hàng.");
-
             int maCuaHang = cuaHang.MaCuaHang;
 
-
-
-
-            //            var rawWarnings = db.Database.SqlQuery<dynamic>(
-            //    "EXEC sp_GetLowStockWarnings"
-            //).ToList();
-
-            //            List<dynamic> filteredWarnings = new List<dynamic>();
-
-            //            foreach (var item in rawWarnings)
-            //            {
-            //                var dict = item as IDictionary<string, object>;
-
-            //                if (dict != null &&
-            //                    dict.ContainsKey("MaCuaHang") &&
-            //                    dict["MaCuaHang"] != null &&
-            //                    Convert.ToInt32(dict["MaCuaHang"]) == maCuaHang)
-            //                {
-            //                    filteredWarnings.Add(item);
-            //                }
-            //            }
-
-            //            ViewBag.Warnings = filteredWarnings;
+            //goi sp de canh bao sap het san pham ton kho
 
             var warnings = db.Database.SqlQuery<WarningViewModel>("EXEC sp_GetLowStockWarnings").ToList();
             var filteredWarnings = warnings.Where(x => x.MaCuaHang == maCuaHang).ToList();
@@ -90,48 +59,10 @@ namespace ECommerceWebsiteMVC.Controllers
             ViewBag.Warnings = filteredWarnings;
 
 
+            //lay san pham thuoc cua hang do
+            var dsSanPham = db.SanPhams.Include("AnhSanPhams").Include("BienTheSanPhams").Where(sp => sp.MaCuaHang == maCuaHang).OrderByDescending(sp => sp.MaSanPham).ToList();
 
-
-
-
-
-
-            // 2. Lấy sản phẩm thuộc cửa hàng đó với các thông tin liên quan
-            var dsSanPham = db.SanPhams
-                              .Include("AnhSanPhams")
-                              .Include("BienTheSanPhams")
-                              .Where(sp => sp.MaCuaHang == maCuaHang)
-                              .OrderByDescending(sp => sp.MaSanPham)
-                              .ToList();
-
-            // 3. Tính doanh số cho mỗi sản phẩm
-            //var productSales = new Dictionary<int, int>();
-
-            //if (dsSanPham.Any())
-            //{
-            //    var allBienTheIds = dsSanPham.SelectMany(sp => sp.BienTheSanPhams?.Select(bt => bt.MaBienThe) ?? new List<int>()).ToList();
-
-            //    if (allBienTheIds.Any())
-            //    {
-            //        var salesData = db.ChiTietDonHangs
-            //            .Include("DonHang")
-            //            .Include("BienTheSanPham")
-            //            .Where(ct => allBienTheIds.Contains(ct.MaBienThe)
-            //                && ct.DonHang != null
-            //                && (ct.DonHang.TrangThaiDonHang == "Đã giao" || ct.DonHang.TrangThaiDonHang == "Đang giao"))
-            //            .ToList()
-            //            .GroupBy(ct => ct.BienTheSanPham?.MaSanPham ?? 0)
-            //            .Where(g => g.Key > 0)
-            //            .Select(g => new { MaSanPham = g.Key, SoLuongBan = g.Sum(x => x.SoLuong) })
-            //            .ToList();
-
-            //        foreach (var sale in salesData)
-            //        {
-            //            productSales[sale.MaSanPham] = sale.SoLuongBan;
-            //        }
-            //    }
-            //}
-            //
+            //goi fn de tinh tong ton kho
             var tonk = new Dictionary<int, int>();
 
             foreach (var sp in dsSanPham)
@@ -141,7 +72,6 @@ namespace ECommerceWebsiteMVC.Controllers
             }
             ViewBag.TonKho = tonk;
 
-            //ViewBag.ProductSales = productSales;
 
             return View(dsSanPham);
         }
@@ -235,6 +165,7 @@ namespace ECommerceWebsiteMVC.Controllers
 
             TempData["UpdateSuccess"] = "Sản phẩm đã được cập nhật.";
             return RedirectToAction("CapNhatSanPham", new { id = sanPham.MaSanPham });
+
         }
 
         public ActionResult ThemSanPham()
