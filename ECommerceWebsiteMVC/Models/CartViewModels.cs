543b15141ee825757377e2540d8c1f37f25af2c0
Tran Nguyen Bao An <baoan056788@gmail.com>
2025-12-05 20:13:54 +0700

    Giao diện khi xóa, kiểm tra định dạng sdt,khi đăng ký mới sẽ tự sinh giỏ hàng rỗng

--- ECommerceWebsiteMVC/Models/CartViewModels.cs
+++ ECommerceWebsiteMVC/Models/CartViewModels.cs
@@ -9,14 +9,28 @@ namespace ECommerceWebsiteMVC.Models
     public class BienTheSanPhamViewModel
     {
         int _MaBienThe;
+        string _TenBienThe;
+        decimal _GiaBan;
+        string _HinhAnh;
         public int MaBienThe { get; set; }
         public string TenBienThe { get; set; }
         public decimal GiaBan { get; set; }
         public string HinhAnh { get; set; }
+        public BienTheSanPhamViewModel() { }    
     }
 
     public class GioHangItemViewModel
     {
+        int _MaCTGH;
+        int _MaBienThe;
+        int _MaCuaHang;
+        string _TenCuaHang; 
+        string _TenSanPham;
+        string _TenBienThe;
+        string _HinhAnh;
+        decimal _DonGia;
+        int _SoLuong;
+        int _SoLuongTonKho; 
         public int MaCTGH { get; set; }
         public int MaBienThe { get; set; }
 
@@ -34,14 +48,20 @@ namespace ECommerceWebsiteMVC.Models
 
         public decimal ThanhTien => DonGia * SoLuong;
 
+
         public List<BienTheSanPhamViewModel> BienTheList { get; set; }
+
+        public GioHangItemViewModel() { }
     }
 
     public class ShopGroup
     {
+        int _MaCuaHang;
+        string _TenCuaHang;
         public int MaCuaHang { get; set; }
         public string TenCuaHang { get; set; }
         public List<GioHangItemViewModel> Items { get; set; }
+        public ShopGroup() { }
     }
 
     public class GioHangViewModel
@@ -51,6 +71,14 @@ namespace ECommerceWebsiteMVC.Models
 
     public class CheckoutItemVM
     {
+        int _MaCTGH;    
+        int _MaBienThe;
+        string _TenSanPham;
+        string _PhanLoai;
+        decimal _DonGia;
+        int _SoLuong;
+        string _HinhAnh;
+
         public int MaCTGH { get; set; }
         public int MaBienThe { get; set; }
         public string TenSanPham { get; set; }
@@ -61,10 +89,20 @@ namespace ECommerceWebsiteMVC.Models
         public string HinhAnh { get; set; }
 
         public decimal ThanhTien => DonGia * SoLuong;
+        public CheckoutItemVM() { }
     }
 
     public class CheckoutViewModel
     {
+        string _TenNguoiNhan;
+        string _SDT;
+        string _DiaChi;
+        string _MaVoucher;
+        decimal _TongTienHang;
+        decimal _PhiVanChuyen;
+        decimal _GiamGia;
+        decimal _TongThanhToan;
+        public CheckoutViewModel() { }
         public string TenNguoiNhan { get; set; }
         public string SDT { get; set; }
         public string DiaChi { get; set; }
